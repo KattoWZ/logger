306b239c2968a3e3c739af69f4edc2ddda3e9e78
KattoWZ <dickyrahadian99dev@gmail.com>
2025-07-04 09:32:17 +0700

    Adding root dir scanner

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 from utils.inputValidator import reqInput as ri
 from utils.titleUI import print_title as pt
 from datetime import datetime
+from pathlib import Path
 import os
 
-log_dir = os.path.join(os.environ['HOME'], "logger", "logs")
+#Function to scan the root folder if the default path is not met
+def find_project_root(marker="main.py"):
+    current = Path(__file__).resolve().parent
+    while current != current.parent:
+        if (current / marker).exists():
+            return current
+        current = current.parent
+    raise FileNotFoundError("Project root not found")
+
+#Get the path of the current script's dir
+BASE_DIR = find_project_root()
+
+#Define logs dir
+log_dir = BASE_DIR / "logs"
+
+#Define utils dir
+utils_dir = BASE_DIR / "utils"
